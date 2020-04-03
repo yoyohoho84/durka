2185a6e6eafcb0ece6a57143007f8e8ad591d3b3
Kirill <yoyohoho84@gmail.com>
2020-04-03 12:26:49 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.bb70386b0075ef0061654d498265a13d.js"
+  "./precache-manifest.8cb209146ee2f15740533ad98547a4f7.js"
 );
 
 self.addEventListener('message', (event) => {
