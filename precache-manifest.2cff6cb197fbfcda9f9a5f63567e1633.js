ca24d79e462141194ff67ef97a70afde2a2fd76d
Kirill <yoyohoho84@gmail.com>
2020-04-04 18:14:09 +0300

    Updates

---
+++ precache-manifest.2cff6cb197fbfcda9f9a5f63567e1633.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d006b79d2479c8ecc69f10b01d99010d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e96340438fbe11d6741c",
+    "url": "./static/css/2.733ba2d0.chunk.css"
+  },
+  {
+    "revision": "9e69106dc4c3ff0d8dfe",
+    "url": "./static/css/main.3cb828c0.chunk.css"
+  },
+  {
+    "revision": "e96340438fbe11d6741c",
+    "url": "./static/js/2.26969d0f.chunk.js"
+  },
+  {
+    "revision": "1201c8656ff5734f581290e1df7d9b22",
+    "url": "./static/js/2.26969d0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e69106dc4c3ff0d8dfe",
+    "url": "./static/js/main.189256a9.chunk.js"
+  },
+  {
+    "revision": "f038ae0613f2fdab979c",
+    "url": "./static/js/runtime-main.3e4555f7.js"
+  },
+  {
+    "revision": "bf9a1c17f3ddb72cffaec36368a4e825",
+    "url": "./static/media/Component 18.bf9a1c17.svg"
+  },
+  {
+    "revision": "4a736e27d2ddd50d64b456a6585efab2",
+    "url": "./static/media/Component 19.4a736e27.svg"
+  },
+  {
+    "revision": "4bf8ce4221b1eeed8e34e1abdc6b54d8",
+    "url": "./static/media/Component 20.4bf8ce42.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.bcc86aa47af0b842c054c7cca69dd8c7.js"
+  "./precache-manifest.2cff6cb197fbfcda9f9a5f63567e1633.js"
 );
 
 self.addEventListener('message', (event) => {
